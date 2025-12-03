b5e3b118cb289032718376f545c3ca859d6abd60
sakeharicharan <Sakeharicharan0123@gmail.com>
2025-12-03 10:12:51 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -8,5 +8,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
 
 export KOPS_STATE_STORE=s3://haricharan.k8s.local
-kops create cluster --name hari.k8s.local --zones ap-south-1a --control-plane-image ami-0d176f79571d18a8f --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-0f918f7e67a3323f0 --node-count=2 --node-size c7i-flex.large
+kops create cluster --name hari.k8s.local --zones ap-south-1a --control-plane-image ami-0d176f79571d18a8f --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-0d176f79571d18a8f --node-count=2 --node-size c7i-flex.large
 kops update cluster --name hari.k8s.local --yes --admin
